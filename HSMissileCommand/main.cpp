b6e98d972afa69fa219c2c28328d78c9d29c6d62
Harrison Statham <harrison.statham@comcast.net>
2016-11-11 19:00:09 -0500

    Initial Commit

---
+++ HSMissileCommand/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HSMissileCommand
+//
+//  Created by Harrison on 11/11/16.
+//  Copyright © 2016 HarrisonStatham. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
